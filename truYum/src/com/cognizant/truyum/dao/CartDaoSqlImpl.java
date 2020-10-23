eed321adaee31493a29567ba6a9095f02445be58
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-23 11:44:04 +0530

    addCartItem completed

--- truYum/src/com/cognizant/truyum/dao/CartDaoSqlImpl.java
+++ truYum/src/com/cognizant/truyum/dao/CartDaoSqlImpl.java
@@ -3,6 +3,7 @@ package com.cognizant.truyum.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.cognizant.truyum.model.MenuItem;
@@ -15,6 +16,17 @@ public class CartDaoSqlImpl implements CartDao {
 
 		try {
 			Connection connection = ConnectionHandler.getConnection();
+			String query = "INSERT INTO CART(CT_USER_ID, CT_MENU_ID) VALUES (?, ?)";
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			
+			preparedStatement.setLong(1, userId);
+			preparedStatement.setLong(2, menuItemId);
+			
+			if(preparedStatement.executeUpdate() > 0) {
+				System.out.println("Query Successful");
+			}else {
+				System.out.println("Query Unsuccessful");
+			}
 			
 		} catch (ClassNotFoundException e) {
 
@@ -28,8 +40,24 @@ public class CartDaoSqlImpl implements CartDao {
 
 	@Override
 	public List<MenuItem> getAllCartItems(long userId) throws CartEmptyException {
-
-		return null;
+		List<MenuItem> menuItemList = new ArrayList<>();
+		
+		try {
+			Connection connection = ConnectionHandler.getConnection();
+			
+			String query = "";
+			PreparedStatement prepareStatement = connection.prepareStatement(query);
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return menuItemList;
 	}
 
 	@Override
